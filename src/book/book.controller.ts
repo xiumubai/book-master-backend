25a167bbc70d5a66b0361ec2afe31f02611f430a
guojianli <1547702880@qq.com>
2024-08-22 16:35:06 +0800

    feat: 图片上传

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -6,18 +6,25 @@ import {
   Put,
   Param,
   Delete,
+  Query,
+  BadRequestException,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import * as path from 'path';
+import { storage } from './book.storage';
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
   @Get('list')
-  async List() {
-    return this.bookService.list();
+  async List(@Query('name') name: string) {
+    return this.bookService.list(name);
   }
 
   @Get(':id')
@@ -38,5 +45,28 @@ export class BookController {
   @Delete('delete/:id')
   async delete(@Param('id') id: string) {
     return this.bookService.delete(+id);
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'uploads',
+      storage: storage,
+      limits: {
+        fileSize: 1024 * 1024 * 3,
+      },
+      fileFilter(req, file, callback) {
+        const extname = path.extname(file.originalname);
+        if (['.png', '.jpg', '.jpeg', '.gif'].includes(extname)) {
+          callback(null, true);
+        } else {
+          callback(new BadRequestException('只能上传图片'), false);
+        }
+      },
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log('file', file);
+    return file.path;
   }
 }
